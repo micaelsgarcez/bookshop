ef44007596a6f600ca979984ad83e5ef52e80931
micaelsgarcez <99693843+micaelsgarcez@users.noreply.github.com>
2022-04-29 05:14:51 -0300

    Adicionado seção banner

---
+++ components/Banner/index.js
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+
+export default function Banner() {
+  return (
+    <div className='block relative'>
+      <Image
+        src='/banner.png'
+        layout='responsive'
+        width={1120}
+        height={702}
+        quality={100}
+      ></Image>
+      <a
+        href='#'
+        className='shadow-promo block absolute top-[4.15vw] left-[55.05vw]'
+      >
+        <Image
+          src='/promo-1.png'
+          layout='fixed'
+          width={149}
+          height={204}
+          quality={100}
+        ></Image>
+      </a>
+      <a
+        href='#'
+        className='shadow-promo block absolute bottom-[3.65vw] right-[-1.2vw] translate-x-[100%]'
+      >
+        <Image
+          src='/promo-2.png'
+          layout='fixed'
+          width={137}
+          height={273}
+          quality={100}
+        ></Image>
+      </a>
+    </div>
+  )
+}
---
+++ components/Nav/index.js
@@ -0,0 +1,69 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Navigation() {
+  return (
+    <nav className='py-11 flex items-center justify-between'>
+      <div className='logo'>
+        <Link href='/'>
+          <h1 className='text-2xl font-bold'>Bookshop</h1>
+        </Link>
+      </div>
+      <ul className='flex items-center'>
+        <li>
+          <a
+            className='text-xs text-[#1C2A39] uppercase bold mr-[2.09vw] hover:text-[#1C2A39] transition-all duration-300 font-black '
+            href='#'
+          >
+            Books
+          </a>
+        </li>
+        <li>
+          <a
+            className='text-xs text-[#5C6A79] uppercase bold mr-[2.09vw] hover:text-[#1C2A39] transition-all duration-300'
+            href='#'
+          >
+            Audiobooks
+          </a>
+        </li>
+        <li>
+          <a
+            className='text-xs text-[#5C6A79] uppercase bold mr-[2.09vw] hover:text-[#1C2A39] transition-all duration-300'
+            href='#'
+          >
+            Stationery & Gifts
+          </a>
+        </li>
+        <li>
+          <a
+            className='text-xs text-[#5C6A79] uppercase bold hover:text-[#1C2A39] transition-all duration-300'
+            href='#'
+          >
+            Blog
+          </a>
+        </li>
+      </ul>
+      <div className='flex items-center'>
+        <a className='mr-[2.09vw]' href='#'>
+          <Image src='/user.svg' layout='fixed' width={12} height={15}></Image>
+        </a>
+        <a className='mr-[2.09vw]' href='#'>
+          <Image
+            src='/search.svg'
+            layout='fixed'
+            width={15}
+            height={14.71}
+          ></Image>
+        </a>
+        <a href='#'>
+          <Image
+            src='/shop-bag.svg'
+            layout='fixed'
+            width={13.68}
+            height={17}
+          ></Image>
+        </a>
+      </div>
+    </nav>
+  )
+}
